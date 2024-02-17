2d0b414550afde88a201f698d88348eb441577ae
aspeckt112 <little.koala5344@fastmail.com>
2024-02-17 22:50:38 +0000

    Added attribute, added samples to verify the attribute behaves as expected.

---
+++ samples/Saucy.Library/ExcludedType.cs
@@ -0,0 +1,7 @@
+using Saucy.Common.Attributes;
+
+namespace Saucy.Library;
+
+// This is excluded by virtue of the fact that there's no suffix registration for it and it's not being
+// explicitly included with the IncludeRegistration attribute
+public class ExcludedType;
---
+++ samples/Saucy.Library/IncludedTypeWithExplicitSingletonScope.cs
@@ -0,0 +1,11 @@
+using Saucy.Common.Attributes;
+using Saucy.Common.Enums;
+
+namespace Saucy.Library;
+
+[IncludeRegistration]
+[UseScope(ServiceScope.Singleton)]
+public class IncludedTypeWithExplicitSingletonScope
+{
+
+}
