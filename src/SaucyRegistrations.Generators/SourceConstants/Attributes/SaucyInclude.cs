2020dcc9eacd53f0417a9933fe863065492fb723
aspeckt112 <little.koala5344@fastmail.com>
2024-04-16 20:33:15 +0100

    Added the ability to ignore an interface when registering a class.

--- src/SaucyRegistrations.Generators/SourceConstants/Attributes/SaucyInclude.cs
+++ src/SaucyRegistrations.Generators/SourceConstants/Attributes/SaucyInclude.cs
@@ -1,19 +1,18 @@
-namespace SaucyRegistrations.Generators.SourceConstants.Attributes
+namespace SaucyRegistrations.Generators.SourceConstants.Attributes;
+
+/// <summary>
+/// The definition of the SaucyInclude attribute.
+/// </summary>
+internal static class SaucyInclude
 {
     /// <summary>
-    /// The definition of the SaucyInclude attribute.
+    /// The value of the SaucyInclude attribute.
     /// </summary>
-    internal static class SaucyInclude
-    {
-        /// <summary>
-        /// The value of the SaucyInclude attribute.
-        /// </summary>
-        internal const string SaucyIncludeAttributeDefinition = $$"""
-                                                                  [System.AttributeUsage(System.AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-                                                                  internal class {{nameof(SaucyInclude)}} : System.Attribute
-                                                                  {
-                                                                      internal {{nameof(SaucyInclude)}}(ServiceScope withScope) { }
-                                                                  }
-                                                                  """;
-    }
+    internal const string SaucyIncludeAttributeDefinition = $$"""
+                                                              [System.AttributeUsage(System.AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+                                                              internal class {{nameof(SaucyInclude)}} : System.Attribute
+                                                              {
+                                                                  internal {{nameof(SaucyInclude)}}(ServiceScope withScope) { }
+                                                              }
+                                                              """;
 }
--- src/SaucyRegistrations.Generators/SourceConstants/Attributes/SaucyIncludeNamespaceWithSuffix.cs
+++ src/SaucyRegistrations.Generators/SourceConstants/Attributes/SaucyIncludeNamespaceWithSuffix.cs
@@ -1,19 +1,18 @@
-namespace SaucyRegistrations.Generators.SourceConstants.Attributes
+namespace SaucyRegistrations.Generators.SourceConstants.Attributes;
+
+/// <summary>
+/// The definition of the SaucyIncludeNamespaceWithSuffix attribute.
+/// </summary>
+internal static class SaucyIncludeNamespaceWithSuffix
 {
     /// <summary>
-    /// The definition of the SaucyIncludeNamespaceWithSuffix attribute.
+    /// The value of the SaucyIncludeNamespaceWithSuffix attribute.
     /// </summary>
-    internal static class SaucyIncludeNamespaceWithSuffix
-    {
-        /// <summary>
-        /// The value of the SaucyIncludeNamespaceWithSuffix attribute.
-        /// </summary>
-        internal const string SaucyIncludeNamespaceWithSuffixAttributeDefinition = $$"""
-                                                                                     [System.AttributeUsage(System.AttributeTargets.Assembly, Inherited = false, AllowMultiple = true)]
-                                                                                     internal class {{nameof(SaucyIncludeNamespaceWithSuffix)}} : System.Attribute
-                                                                                     {
-                                                                                         internal {{nameof(SaucyIncludeNamespaceWithSuffix)}}(string suffix, ServiceScope defaultScope) { }
-                                                                                     }
-                                                                                     """;
-    }
+    internal const string SaucyIncludeNamespaceWithSuffixAttributeDefinition = $$"""
+                                                                                 [System.AttributeUsage(System.AttributeTargets.Assembly, Inherited = false, AllowMultiple = true)]
+                                                                                 internal class {{nameof(SaucyIncludeNamespaceWithSuffix)}} : System.Attribute
+                                                                                 {
+                                                                                     internal {{nameof(SaucyIncludeNamespaceWithSuffix)}}(string suffix, ServiceScope defaultScope) { }
+                                                                                 }
+                                                                                 """;
 }
