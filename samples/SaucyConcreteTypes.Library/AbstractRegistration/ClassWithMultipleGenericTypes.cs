8df1415e23f9c5b663c6290b4d0e669e31c260c2
aspeckt112 <little.koala5344@fastmail.com>
2024-05-12 12:30:09 +0100

    Added unit tests to cover all key behaviours of the generator.

--- samples/SaucyConcreteTypes.Library/AbstractRegistration/ClassWithMultipleGenericTypes.cs
+++ samples/SaucyConcreteTypes.Library/AbstractRegistration/ClassWithMultipleGenericTypes.cs
@@ -1,6 +1,7 @@
 namespace SaucyConcreteTypes.Library.AbstractRegistration;
 
 [SaucyInclude(ServiceScope.Singleton)]
+[SaucyRegisterAbstractClass]
 public class ClassWithMultipleGenericTypes : AbstractBaseClassWithMultipleGenericTypes<int, ComplexType>
 {
 
