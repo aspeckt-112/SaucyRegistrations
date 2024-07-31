9dd057c6857fa1e100e1a5e03ef650c6d110ada2
aspeckt-112 <little.koala5344@fastmail.com>
2024-07-31 21:44:45 +0100

    Added stuff missing from previous commit.

---
+++ samples/ServiceScopeOverrideSample/Program.cs
@@ -0,0 +1,14 @@
+﻿// Check the generated code.
+
+using Microsoft.Extensions.DependencyInjection;
+
+using ServiceScopeOverrideSample.ServiceCollectionExtensions;
+
+IServiceCollection serviceCollection = new ServiceCollection();
+serviceCollection.AddServiceScopeOverrideSampleServices();
+
+// Notice that everything is the namespace (AssemblyInfo.cs) is registered with the scope Singleton, but ClassTwo has
+// [SaucyInclude(ServiceScope.Transient)], which means it'll be registered as Transient.
+// If you find yourself doing this often, you might want to consider the design of your application.
+
+Console.ReadLine();
